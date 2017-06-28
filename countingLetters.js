ce0fd965b28c3d9c8d969f094d3b38ae64e49619
Nikolai <nikolaigauer@gmail.com>
2017-06-28 23:21:01 +0000

    IT HAS BEEN DONE

--- countingLetters.js
+++ countingLetters.js
@@ -1,5 +1,19 @@
-function countLetters(string) {
-  var stringSplit = string.split([''[string.length]]);
-  return stringSplit
+function countLetters(words) {
+
+var letterCounts = {};
+
+for (var i = 0; i < words.length; i++) {
+  var letter = words[i]
+    if (letter === ' ') {
+    continue;
+    }
+    if (letterCounts[letter] !== undefined) {
+       letterCounts[letter] += 1
+    } else {
+      letterCounts[letter] = 1;
+      }
+    }
+    return letterCounts;
 }
-console.log(countLetters("Lighthouse"))
+console.log(countLetters("LiTTle BiG")) // logs any string to the console, separated and lower case
+
