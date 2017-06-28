fcb8787d64ff5f32ec57774da39f59b63f9fdae0
Nikolai <nikolaigauer@gmail.com>
2017-06-28 20:37:45 +0000

    YEAH

--- countingLetters.js
+++ countingLetters.js
@@ -1,3 +1,5 @@
-WOOOOOOOOGDALGALDKGA:LDJGHA:DLGJHADG
-
-ADUGH
+function countLetters(string) {
+  var stringSplit = string.split([''[string.length]]);
+  return stringSplit
+}
+console.log(countLetters("Lighthouse"))
